28477563e30c484b4f4853cd45d5d01bc52a2edc
s1171632 <s1171632@student.windesheim.nl>
2021-11-26 12:43:57 +0100

    Aanpassingen winkelmand

--- cart.php
+++ cart.php
@@ -8,13 +8,17 @@ include "cartfuncties.php";
 
             <?php
             $cart = getCart();
+            $totalPrice = 0;
             foreach ($cart as $id => $quantity) {
                 $StockItem = getStockItem($id, $databaseConnection);
 
                 $name = $StockItem['StockItemName'];
 
                 $StockItemImage = getStockItemImage($id, $databaseConnection);
-                $image = $StockItemImage[0]['ImagePath'];
+
+                if (!empty($StockItemImage)) {
+                    $image = $StockItemImage[0]['ImagePath'];
+                }
 
                 $sellPrice = round($StockItem['SellPrice'], 2);
 
@@ -23,27 +27,52 @@ include "cartfuncties.php";
                 <div class="product-in-cart">
                     <div class="image">
                         <div id="ImageFrame"
-                             style="background-image: url('Public/StockItemIMG/<?= $image ?>'); background-size: 300px; background-repeat: no-repeat; background-position: center;"></div>
+                             style="background-image: url('Public/StockItemIMG/<?php print $image ?>'); background-size: 300px; background-repeat: no-repeat; background-position: center;"></div>
                     </div>
+                    <?php if (!empty($_POST["submitCount"])) {
+                        $stockItemID = $_POST["stockItemID"];
+                        updateCart($stockItemID, $_POST["aantal"]);
+                        $quantity = $_POST['aantal'];
+                        $total = $sellPrice * $quantity;
+                    }
+                    ?>
+
                     <div class="description">
-                        <div class="name">Product naam: <a href="view.php?id=<?= $id ?>"><?= $name ?></a></div>
-                        <div class="price">Prijs: <?= '€' . $sellPrice ?></div>
-                        <div class="quantity">Aantal: <?= $quantity ?></div>
-                        <div class="total">Totaal: <?= '€' . $total ?></div>
+                        <div class="name">Product naam: <a
+                                    href="view.php?id=<?php print $id ?>"><?php print $name ?></a></div>
+                        <div class="price">Prijs: <?php print '€' . $sellPrice ?></div>
+                        <div class="quantity">Aantal:
+                            <form method="post" class="submited">
+                                <input type="number" name="stockItemID"
+                                       value="<?php print($StockItem['StockItemID']) ?>" hidden><input type="number"
+                                                                                                       name="aantal"
+                                                                                                       value="<?php print $quantity ?>"
+                                                                                                       min="1"/> <input
+                                        name="submitCount" type="submit" value="Aanpassen"/>
+
+                            </form>
+                        </div>
+                        <div class="total">Totaal: <?php print '€' . $total ?></div>
+                        <?php
+                        if (isset($_POST["submit"])) {
+                            $stockItemID = $_POST["stockItemID"];
+                            removeProductFromCart($stockItemID);
+
+                            print("Product verwijderd uit <a href='cart.php'> winkelmandje!</a>");
+                        }
+                        ?>
                         <form method="post">
-                            <input type="number" name="stockItemID" value="<?php print($StockItem['StockItemID']) ?>" hidden>
+                            <input type="number" name="stockItemID" value="<?php print($StockItem['StockItemID']) ?>"
+                                   hidden>
                             <input type="submit" name="submit" value="Verwijder uit winkelmandje">
-                            <?php
-                            if (isset($_POST["submit"])) {
-                                $stockItemID = $_POST["stockItemID"];
-                                removeProductFromCart($stockItemID);
-                                print("Product verwijderd uit <a href='cart.php'> winkelmandje!</a>");
-                            }
-                            ?>
                         </form>
                     </div>
                 </div>
                 <?php
+            }
+            if (!empty($total)) {
+                $totalPrice += $total;
+                print "<p>Totaalprijs Winkelmandje: €" . $totalPrice . "</p>";
             }
             ?>
         </div>
--- cartfuncties.php
+++ cartfuncties.php
@@ -32,10 +32,34 @@ function removeProductFromCart($stockItemID)
     $cart = getCart();                          // eerst de huidige cart ophalen
 
     if (array_key_exists($stockItemID, $cart)) {  //controleren of $stockItemID(=key!) al in array staat
-        $cart[$stockItemID] =- 1;                   //zo ja:  aantal met 1 verhogen
+        unset($cart[$stockItemID]);
+
+
+	    //zo ja:  aantal met 1 verhogen
     } else {
         $cart[$stockItemID] = 1;                    //zo nee: key toevoegen en aantal op 1 zetten.
     }
 
     saveCart($cart);                            // werk de "gedeelde" $_SESSION["cart"] bij met de bijgewerkte cart
+}
+
+function updateCart($stockItemID, $amount)
+{
+	$cart = getCart();                          // eerst de huidige cart ophalen
+
+	if (array_key_exists($stockItemID, $cart)) {  //controleren of $stockItemID(=key!) al in array staat
+		if($amount <= 0){
+			unset($cart[$stockItemID]);
+		}
+		else {
+			$cart[$stockItemID] = $amount;
+		}
+															//zo ja:  aantal met 1 verhogen
+	} else {
+		$cart[$stockItemID] = 1;                    //zo nee: key toevoegen en aantal op 1 zetten.
+	}
+
+	saveCart($cart);
+	return $amount;
+									// werk de "gedeelde" $_SESSION["cart"] bij met de bijgewerkte cart
 }
