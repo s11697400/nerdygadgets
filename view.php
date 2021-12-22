935712ec03ce66953bc0cb730ec8b74f82250b2d
Thijs Soepenberg <tsoepenberg@gmail.com>
2021-12-22 13:22:16 +0100

    merge conversie

--- view.php
+++ view.php
@@ -1,7 +1,7 @@
 <!-- dit bestand bevat alle code voor de pagina die één product laat zien -->
 <?php
 include __DIR__ . "/header.php";
-include "cartfuncties.php";
+// include "cartfuncties.php";
 include "viewfunctions.php";
 
 $StockItem = getStockItem($_GET['id'], $databaseConnection);
