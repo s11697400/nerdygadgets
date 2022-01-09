fa11a3365a741bd30f9998349cf1056863250fe6
Olevk <olevankuiken@gmail.com>
2022-01-09 09:08:48 +0100

    postcode check

--- register.php
+++ register.php
@@ -2,10 +2,18 @@
 
 include __DIR__ . "/header.php";
 
-if(isset($_POST['submit'])){
-   $customer = insertCustomer($_POST['name'], $_POST['password'], $_POST['phone'], $_POST['adress'], $_POST['postcode'], $databaseConnection);
-    if($customer){
-        print "Account is toegevoegd";
+if(isset($_POST['submit'])) {
+    $postcode = $_POST ['postcode'];
+
+    $viercijferstweeletters = preg_match('/^[1-9][0-9]{3} ?[a-zA-Z]{2}$/', $postcode);
+
+    if (!$viercijferstweeletters) {
+        print 'Het is geen geldige postcode!';
+    } else {
+        $customer = insertCustomer($_POST['name'], $_POST['password'], $_POST['phone'], $_POST['adress'], $_POST['postcode'], $databaseConnection);
+        if ($customer) {
+            print "Account is toegevoegd";
+        }
     }
 }
 ?>
@@ -16,8 +24,8 @@ if(isset($_POST['submit'])){
         <input type="text" name="phone" placeholder="Telefoonnummer" required/>
         <input type="text" name="adress" placeholder="Adress" required/>
         <input type="text" name="postcode" placeholder="Postcode" required/>
-        <div>  <input type="checkbox" name="privacy" required/>
-  <label for="privacy">Ik ga akkoord met het privacybeleid</label></div>
+        <div><input type="checkbox" name="privacy" required/>
+            <label for="privacy">Ik ga akkoord met het privacybeleid</label></div>
         <input type="submit" name="submit" value="Registreren"/>
     </form>
 </div>
